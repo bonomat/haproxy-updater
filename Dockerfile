04fde5d371cd508573dbf793d5a1dc74d818f6f1
Philipp Hoenisch <philipp@hoenisch.at>
2015-04-01 10:17:38 +1100

    working version,

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,9 @@ FROM    centos:centos6
 MAINTAINER Philipp Hoenisch philipp@hoenisch.at
 
 # Enable EPEL for Node.js
-RUN     rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+#RUN     rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 # Install Node.js and npm
-RUN     yum install -y npm
+#RUN     yum install -y npm vim
 
 # Bundle app source
 COPY . /src
--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 var express = require('express');
+var multer = require('multer');
 
-// Constants
-var PORT = 3000;
-
-// App
 var app = express();
+var completed = false;
+
+app.use(multer({ dest: './uploads',
+
+	rename: function(fieldname, filename) {
+	   return 'haproxy-new.cfg';
+//		return filename + Date.now();
+	},
+
+	onFileUploadStart: function(file) {
+		console.log(file.originalname + 'loading...')
+	},
+
+	onFileUploadComplete: function(file) {
+		console.log(file.originalname + 'uploaded to ' + file.path);
+		completed = true;
+	}
+
+}));
+
+app.use(express.static(__dirname + '/'));
+
 app.get('/', function (req, res) {
-  res.send('Hello world\n We are up and running on ' + PORT);
+	res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+app.post('/api/files', function (req, res) {
+	if (completed === true) {
+		console.log(req.files);
+		res.end('File uploaded. Step back to upload more.');
+	}
+});
+
+app.listen(3000, function() {
+	console.log("Magic happens on port 3000");
+});
