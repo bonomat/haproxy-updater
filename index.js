33c0ea90f6ac2042564a47f52c58963e84dbe0b5
Philipp Hoenisch <philipp@hoenisch.at>
2015-04-01 15:31:17 +1100

    update haconfig and restart service

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var multer = require('multer');
+var fs = require("fs");
 
 var app = express();
 var completed = false;
@@ -7,8 +8,7 @@ var completed = false;
 app.use(multer({ dest: './uploads',
 
 	rename: function(fieldname, filename) {
-	   return 'haproxy-new.cfg';
-//		return filename + Date.now();
+	   return 'haproxy-new';
 	},
 
 	onFileUploadStart: function(file) {
@@ -16,11 +16,52 @@ app.use(multer({ dest: './uploads',
 	},
 
 	onFileUploadComplete: function(file) {
-		console.log(file.originalname + 'uploaded to ' + file.path);
+		console.log(file.originalname + ' uploaded to ' + file.path);
 		completed = true;
-	}
+		copyFile(file.path, '/etc/haproxy/haproxy.cfg', function(err) {
+         if(err) {
+            console.log(err);            
+         } else {
+            console.log('haproxy updated');
+            exec('service haproxy restart', function (error, stdout, stderr) {
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+                if (error !== null) {
+                  console.log('exec error: ' + error);
+                } else { 
+                  console.log('restarted') 
+                }
+            });
 
+         }
+      });
+	}
 }));
+
+function copyFile(source, target, cb) {
+  var cbCalled = false;
+
+  var rd = fs.createReadStream(source);
+  rd.on("error", function(err) {
+    done(err);
+  });
+  var wr = fs.createWriteStream(target);
+  wr.on("error", function(err) {
+    done(err);
+  });
+  wr.on("close", function(ex) {
+    done();
+  });
+  rd.pipe(wr);
+
+  function done(err) {
+    if (!cbCalled) {
+      cb(err);
+      cbCalled = true;
+    }
+  }
+}
+
 
 app.use(express.static(__dirname + '/'));
 
